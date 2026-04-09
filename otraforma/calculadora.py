3a20d7ba4170ab3cf8b59b019477eba3a7e89a95
joacopiolapiola <joaquinarinbritos@gmail.com>
2026-04-08 21:34:56 -0300

    empece el array de los botones no pude hacer la interfaz

--- otraforma/calculadora.py
+++ otraforma/calculadora.py
@@ -1,18 +1,5 @@
-#poner botones
-#layout = QGridLayout()
-
-#layout.addWidget(, 0, 3)
-#layout.addWidget(QPushButton("uno"), 0, 0)
-#layout.addWidget(QPushButton("dos"), 0, 1)
-#layout.addWidget(QPushButton("tres"), 0, 2)
-#layout.addWidget(QPushButton("uno"), 0, 0)
-#layout.addWidget(Color("orange"), 2, 2)
-#layout.addWidget(Color("blue"), 3, 0)
 
 #hacer lector 
-#mostrar = QLabel(lector)
-#mostrar.setText(lector))
-#lector=lector+""
 
 #calcular
 
@@ -21,21 +8,52 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout
+
+lector="0"
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("calculadora")
-        
-        layout.setContentsMargins(0,0,0,0)
-        layout.setSpacing(20)
-        
+
+        # widget central
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+
+
+        # layout
         layout = QGridLayout()
-        layout.addWidget(QPushButton("uno"), 0, 0)
-        layout.addWidget(QPushButton("dos"), 0, 1)
-        layout.addWidget(QPushButton("tres"), 0, 2)
-        layout.addWidget(QPushButton("uno"), 0, 0)
+        central_widget.setLayout(layout)
+
+        botones = ["uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve","+","-","/","*","="]
+        
+        for b in botones:
+            var=0
+            if(botones.index(b) % 3 == 0 ):
+                var=var+1
+            b=layout.addWidget(QPushButton(b), botones.index(b),var)
+
+        
+        # uno=layout.addWidget(QPushButton("uno"), 0, 0)
+        # dos=layout.addWidget(QPushButton("dos"), 0, 1)
+        # tres=layout.addWidget(QPushButton("tres"), 0, 2)
+        # cuatro=layout.addWidget(QPushButton("cuatro"), 1, 0)
+        # cinco=layout.addWidget(QPushButton("cinco"), 1, 1)
+        # seis=layout.addWidget(QPushButton("seis"), 1, 2)
+        # siete=layout.addWidget(QPushButton("siete"), 2, 0)
+        # ocho=layout.addWidget(QPushButton("ocho"), 2, 1)
+        # nueve=layout.addWidget(QPushButton("nueve"), 2, 2)
+        # mas=layout.addWidget(QPushButton("+"), 0, 3)
+        # menos=layout.addWidget(QPushButton("-"), 1, 3)
+        # multi=layout.addWidget(QPushButton("X"), 2, 3)
+        # dividir=layout.addWidget(QPushButton("/"), 3, 3)
+        # igual=layout.addWidget(QPushButton("="), 3, 4)
+
+        #conectar numeros y pantalla
+        #uno.clicked.connect(lector=lector+"1")     
+
 
 
 
